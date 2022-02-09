27c0537229470aeddd2a44ced0df34402adf690e
Dirk Faust <dirk@faustbande.de>
2022-02-09 16:03:37 +0100

    Extend documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,33 @@
+//! This crate provides a solution to evaluate comparison-texts against given metric-values - for example to evaluate
+//! IoT-Triggers against a collection of comparisons ([Sequence] here).
+//!
+//! The comparisons can be written like normal rust-code ("foo > 2 && bar != 42 || (baz == 47111 && barg <= 99)"). Comparisons
+//! can be made against String, Numeric (internally f64), Time (internally NaiveTime) and Duration (represented in humantime).
+//!
+//! Value-Lookup is made through a given [Resolver] internally so you are open to use what ever you like in the background.
+
 use anyhow::Result;
 
+/// Compare [Value] against [Value]
 pub mod compare;
+/// Helper-Object to use [HashMap] as [Resolver]
 pub mod mapresolver;
+/// Parser to generate [Sequence] from a given text
 pub mod parser;
-pub mod sequence;
-pub mod value;
-
+/// Resolves name to [Value]
 pub mod resolver;
+/// Sequence of comparisons
+pub mod sequence;
+/// Solves [Sequence]
 pub mod solver;
+/// A generic value
+pub mod value;
 
 pub use mapresolver::MapResolver;
 pub use resolver::Resolver;
 pub use value::Value;
 
+/// Evaluate `sequence` with the given [Resolver] resolver to a final bool-result
 pub fn evaluate<'a>(
     sequence: impl AsRef<str>,
     resolver: &'a impl resolver::Resolver,
@@ -29,6 +44,7 @@ pub mod async_solver;
 pub use async_resolver::AsyncResolver;
 
 #[cfg(feature = "async")]
+/// Async-version of 'evaluate'
 pub async fn evaluate_async<'a>(
     sequence: impl AsRef<str>,
     resolver: &'a impl async_resolver::AsyncResolver,
